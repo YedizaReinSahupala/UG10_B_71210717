49e8d39f874505f710809e7f2936f05749de3f42
YedizaReinSahupala <93330433+YedizaReinSahupala@users.noreply.github.com>
2021-11-19 06:27:31 +0900

    1

---
+++ 1_B_71210717.py
@@ -0,0 +1,22 @@
+Nilai = input("Masukan nilai huruf PrakTekKom anda: ")
+print("==============================")
+if Nilai == "A":
+    print("Rentang nilai PrakTekKom anda adalah 85 - 100")
+elif Nilai == "A-":
+    print("Rentang nilai PrakTekKom anda adalah 80 - 84")
+elif Nilai == "B+":
+    print("Rentang nilai PrakTekKom anda adalah 75 - 79")
+elif Nilai == "B":
+    print("Rentang nilai PrakTekKom anda adalah 70 - 74")
+elif Nilai == "B-":
+    print("Rentang nilai PrakTekKom anda adalah 65 - 69")
+elif Nilai == "C+":
+    print("Rentang nilai PrakTekKom anda adalah 60 - 64")
+elif Nilai == "C":
+    print("Rentang nilai PrakTekKom anda adalah 55 - 59")
+elif Nilai == "D":
+    print("Rentang nilai PrakTekKom anda adalah 45 - 49")
+elif Nilai == "E":
+    print("Rentang nilai PrakTekKom anda adalah 0")
+else:
+    print("Inputan anda salah atau nilai huruf tidak ada pada Standar Nilai")
